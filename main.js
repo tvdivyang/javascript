7a823092aaa972dc01b3c6e3fe85044f0b00ea1f
tvdivyang <divyang@techvoot.com>
2023-01-24 19:33:56 +0530

    event validation

--- main.js
+++ main.js
@@ -7,43 +7,86 @@ let textarea = document.getElementById("textarea");
 let msg_titel = document.getElementById("msg_titel");
 let msg_email = document.getElementById("msg_email");
 let msg_number = document.getElementById("msg_number");
+let msg_date = document.getElementById("msg_date");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
+let mobilecheck = /^[789][0-9]{9}$/;
+let username = /^[A-Za-z][A-Za-z0-9_]{4,20}$/;
+let gmailcheck = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     formvalidation(); 
 });
-let formvalidation = () => {
-    if (textInput.value === "") {
-        console.log("failure");
-        msg_titel.innerHTML = "task cannot blank";
-        return false;
-    }if(emailInput.value == ""){
-        msg_email.innerHTML = "please fill the email id field";
-        return false;   
-    }if(phoneInput.value == ""){
-        msg_number.innerHTML = "please enter your number";
-        return false;
-    }if(isNaN(phoneInput.value)){
-        msg_number.innerHTML = "enter only numeric number";
-        return false;
-    }if((phoneInput.value.length<10) || (phoneInput.value.length >= 11)){
-        msg_number.innerHTML = "Mobile number must be 10 number";
-        return false;
-    }if((phoneInput.value.charAt(0) !=9) && (phoneInput.value.charAt(0) !=8) && (phoneInput.value.charAt(0) !=7)){
-        msg_number.innerHTML = "Mobile number must be start 9,8,7";
-        return false;
-    }
 
-    else{
-        console.log("success"); 
-        msg_titel.innerHTML = "";
-        acceptData();
-        add.setAttribute("data-bs-dismiss","modal");
-        add.click();
-    }
+let formvalidation = () => { 
+if(username.test(textInput.value)){
+    msg_titel.innerHTML ="";
+} 
+else if(mobilecheck.test(phoneInput.value)){
+    msg_number.innerHTML = "";
+}
+else if(gmailcheck.test(emailInput.value)){
+    msg_email.innerHTML = "";
+}
+else{
+    msg_number.innerHTML = "please enter valid number";
+    msg_titel.innerHTML = "task cannot blank";
+    msg_email.innerHTML = "please enter valid email";
+    console.log("success"); 
+    return false;
+}
+acceptData();
+add.setAttribute("data-bs-dismiss","modal");
+add.click();
 };
+
+document.getElementById("form").onkeyup = function() {myFunction()};
+
+function myFunction() {
+    if(!!textInput.value){
+        msg_titel.innerHTML = "";
+    }
+    if(!!phoneInput.value){
+        msg_number.innerHTML = "";
+    }
+    if(!!emailInput.value){
+        msg_email.innerHTML = "";
+    }
+  
+   }
+
+
+// let formvalidation = () => {
+//     if (textInput.value === "") {
+//         console.log("failure");
+//         msg_titel.innerHTML = "task cannot blank";
+//         return false;
+//     }if(emailInput.value == ""){
+//         msg_email.innerHTML = "please fill the email id field";
+//         return false;   
+//     }if(phoneInput.value == ""){
+//         msg_number.innerHTML = "please enter your number";
+//         return false;
+//     }if(isNaN(phoneInput.value)){
+//         msg_number.innerHTML = "enter only numeric number";
+//         return false;
+//     }if((phoneInput.value.length<10) || (phoneInput.value.length >= 11)){
+//         msg_number.innerHTML = "Mobile number must be 10 number";
+//         return false;
+//     }if((phoneInput.value.charAt(0) !=9) && (phoneInput.value.charAt(0) !=8) && (phoneInput.value.charAt(0) !=7)){
+//         msg_number.innerHTML = "Mobile number must be start 9,8,7";
+//         return false;
+//     }
+
+//     else{
+//         console.log("success"); 
+//         msg_titel.innerHTML = "";
+//         acceptData();
+//         add.setAttribute("data-bs-dismiss","modal");
+//         add.click();
+//     }
+// };
 
 let data = {};
 
